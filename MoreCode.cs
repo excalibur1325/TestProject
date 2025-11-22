f088ba22cc9578b0bd8ac3e126adc392ff84605d
Darren Cruz <excalibur.dc1325@gmail.com>
2025-11-22 15:58:40 -0500

    Changed the name of the file and added new code

--- MoreCode.cs
+++ MoreCode.cs
@@ -1,4 +1,4 @@
-Random dice = new Random();
+/*Random dice = new Random();
 
 int roll1 = dice.Next(1, 7);
 int roll2 = dice.Next(1, 7);
@@ -38,4 +38,31 @@ else if (total == 7)
 else
 {
     Console.WriteLine("You win a kitten!");
+}*/
+
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
+else if(daysUntilExpiration <=5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration <=10)
+{
+    Console.WriteLine("Your subscription will expire soon. You should probably renew to keep your rate.");
+}
+if (discountPercentage > 0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
 }
