7eca780d6975e97e4654487bf2dd90f1b1f9e5ca
Darren Cruz <excalibur.dc1325@gmail.com>
2025-11-22 16:53:17 -0500

    Commiting to move to another project

--- obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6c4d607af80e188adae031379970295720a8c09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f088ba22cc9578b0bd8ac3e126adc392ff84605d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
